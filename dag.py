887f58cbae5d7587d7d091cd2817dd1c48014cdb
Muhamed Tuo <mtuo@pop-os.localdomain>
2022-02-15 20:11:04 +0100

    fix: hdfs put error

--- dag.py
+++ dag.py
@@ -21,8 +21,14 @@ dag = DAG(
 
 @task(task_id='download_raw_data', dag=dag)
 def download_raw_data(year, month, day):
-    raw_data_path = "/groupe8/raw/data.csv"
+    for rm_path in ["/groupe8/clean/data.parquet", "/groupe8/clean/final.parquet"]:
+        try:
+            rm = Popen(["hdfs", "dfs", "-rm", rm_path], stdin=PIPE, bufsize=-1)
+            rm.communicate()
+        except:
+            pass
 
+    raw_data_path = "/groupe8/raw/data.csv"
     url = f'https://opendata.paris.fr/api/v2/catalog/datasets/comptages-routiers-permanents/exports/csv?refine=t_1h%3A{year}%2F{month}%2F{day}&timezone=UTC'
     r = requests.get(url, allow_redirects=True)
     open(f'/tmp/data-{year}-{month}-{day}.csv', 'wb').write(r.content)
